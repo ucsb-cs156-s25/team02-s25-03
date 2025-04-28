bf83fa1168584f9826d258ef5768b321efd9d3a4
Karena Lai <65944717+klai890@users.noreply.github.com>
2025-04-28 16:35:08 -0400

    Add articlesFixture.js

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneDate: {
+      id: 1,
+      title: 'Some Article Title',
+      url: 'https://gmail.com',
+      explanation: 'The article is actually Gmail',
+      email: 'karena_lai@ucsb.edu',
+      dateAdded: "2022-01-02T12:00:00",
+    },
+    threeDates: [
+        {
+            id: 1,
+            title: 'Some Article Title',
+            url: 'https://gmail.com',
+            explanation: 'The article is actually Gmail',
+            email: 'karena_lai@ucsb.edu',
+            dateAdded: "2022-01-02T12:00:00",
+        },
+        {
+            id: 2,
+            title: 'How to Be a Code Editor',
+            url: 'https://youtube.com',
+            explanation: 'Step-by-step guide on how to become a code editor',
+            email: 'youremail@gmail.com',
+            dateAdded: "2024-04-28T12:00:00",
+          },
+          {
+            id: 3,
+            title: 'How to Sleep',
+            url: 'https://wikihow.com',
+            explanation: 'A step-by-step guide on how to fall asleep',
+            email: 'urmom@gmail.com',
+            dateAdded: "2025-04-27T15:03:00",
+          },
+    ],
+  };
+  
+  export { articlesFixtures };
+  
