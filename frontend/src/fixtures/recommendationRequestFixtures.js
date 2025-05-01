7f9bd6309e93d2f63aab832b5335d1f7ed932da7
Daisy <wanqian@ucsb.edu>
2025-05-01 16:46:56 -0700

    run npm format

--- frontend/src/fixtures/recommendationRequestFixtures.js
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -1,5 +1,15 @@
 const recommendationRequestFixtures = {
-    oneDate: {
+  oneDate: {
+    id: 1,
+    requesterEmail: "wanqian@ucsb.edu",
+    professorEmail: "phtcon@ucsb.edu",
+    explanation: "testing",
+    dateRequested: "1111-11-11T11:11:11",
+    dateNeeded: "1111-11-11T11:11:11",
+    done: true,
+  },
+  threeDates: [
+    {
       id: 1,
       requesterEmail: "wanqian@ucsb.edu",
       professorEmail: "phtcon@ucsb.edu",
@@ -8,35 +18,25 @@ const recommendationRequestFixtures = {
       dateNeeded: "1111-11-11T11:11:11",
       done: true,
     },
-    threeDates: [
-      {
-        id: 1,
-        requesterEmail: "wanqian@ucsb.edu",
-        professorEmail: "phtcon@ucsb.edu",
-        explanation: "testing",
-        dateRequested: "1111-11-11T11:11:11",
-        dateNeeded: "1111-11-11T11:11:11",
-        done: true,
-      },
-      {
-        id: 2,
-        requesterEmail: "wanqian@ucsb.edu",
-        professorEmail: "phtcon@ucsb.edu",
-        explanation: "best",
-        dateRequested: "1111-11-11T11:11:11",
-        dateNeeded: "1111-11-11T11:11:11",
-        done: false,
-      },
-      {
-        id: 3,
-        requesterEmail: "wanqian@ucsb.edu",
-        professorEmail: "phtcon@ucsb.edu",
-        explanation: "nest",
-        dateRequested: "1111-11-11T11:11:11",
-        dateNeeded: "1111-11-11T11:11:11",
-        done: false,
-      },
-    ],
-  };
-  
-  export { recommendationRequestFixtures };
+    {
+      id: 2,
+      requesterEmail: "wanqian@ucsb.edu",
+      professorEmail: "phtcon@ucsb.edu",
+      explanation: "best",
+      dateRequested: "1111-11-11T11:11:11",
+      dateNeeded: "1111-11-11T11:11:11",
+      done: false,
+    },
+    {
+      id: 3,
+      requesterEmail: "wanqian@ucsb.edu",
+      professorEmail: "phtcon@ucsb.edu",
+      explanation: "nest",
+      dateRequested: "1111-11-11T11:11:11",
+      dateNeeded: "1111-11-11T11:11:11",
+      done: false,
+    },
+  ],
+};
+
+export { recommendationRequestFixtures };
