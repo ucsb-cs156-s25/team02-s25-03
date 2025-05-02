2c07ad07eac95c9d11e6a9cb9ee632f9cef318ed
daisyzhongg <124753244+daisyzhongg@users.noreply.github.com>
2025-05-01 19:30:54 -0700

    Update recommendationRequestFixtures.js

--- frontend/src/fixtures/recommendationRequestFixtures.js
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -1,42 +1,42 @@
-const recommendationRequestFixtures = {
-  oneDate: {
-    id: 1,
-    requesterEmail: "wanqian@ucsb.edu",
+const RecommendationRequestFixtures = {
+  oneRecommendationRequest: {
+    id: 2,
+    requestorEmail: "wanqian@ucsb.edu",
     professorEmail: "phtcon@ucsb.edu",
-    explanation: "testing",
+    explanation: "testing ",
     dateRequested: "1111-11-11T11:11:11",
     dateNeeded: "1111-11-11T11:11:11",
-    done: true,
+    done: false,
   },
-  threeDates: [
-    {
-      id: 1,
-      requesterEmail: "wanqian@ucsb.edu",
-      professorEmail: "phtcon@ucsb.edu",
-      explanation: "testing",
-      dateRequested: "1111-11-11T11:11:11",
-      dateNeeded: "1111-11-11T11:11:11",
-      done: true,
-    },
+  threeRecommendationRequest: [
     {
       id: 2,
-      requesterEmail: "wanqian@ucsb.edu",
+      requestorEmail: "wanqian@ucsb.edu",
       professorEmail: "phtcon@ucsb.edu",
-      explanation: "best",
+      explanation: "testing",
       dateRequested: "1111-11-11T11:11:11",
       dateNeeded: "1111-11-11T11:11:11",
       done: false,
     },
     {
       id: 3,
-      requesterEmail: "wanqian@ucsb.edu",
+      requestorEmail: "wanqian@ucsb.edu",
       professorEmail: "phtcon@ucsb.edu",
       explanation: "nest",
       dateRequested: "1111-11-11T11:11:11",
       dateNeeded: "1111-11-11T11:11:11",
       done: false,
     },
+    {
+      id: 4,
+      requestorEmail: "wanqian@ucsb.edu",
+      professorEmail: "fgibou@ucsb.edu",
+      explanation: "best",
+      dateRequested: "1111-11-11T11:11:11",
+      dateNeeded: "1111-11-11T11:11:11",
+      done: true,
+    },
   ],
 };
 
-export { recommendationRequestFixtures };
+export { RecommendationRequestFixtures };
