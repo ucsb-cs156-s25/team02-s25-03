b5f6e15eb059e4c3a7feb1366d65bdfbe0e35edd
Saahas Buricha <sburicha@ucsb.edu>
2025-05-02 15:48:32 -0700

    pc- WIP fix formatting

--- frontend/src/fixtures/helpRequestsFixtures.js
+++ frontend/src/fixtures/helpRequestsFixtures.js
@@ -1,45 +1,43 @@
 const helpRequestFixtures = {
+  oneHelpRequest: {
+    id: 1,
+    requesterEmail: "sburicha@ucsb.edu",
+    teamId: "s22-5pm-3\t",
+    tableOrBreakoutRoom: "5",
+    requestTime: "2022-04-20T17:35:00Z",
+    explanation: "DokkuIssue",
+    solved: false,
+  },
 
-    oneHelpRequest: {
-        "id": 1,
-        "requesterEmail": "sburicha@ucsb.edu",
-        "teamId": "s22-5pm-3\t",
-        "tableOrBreakoutRoom": "5",
-        "requestTime": "2022-04-20T17:35:00Z",
-        "explanation": "DokkuIssue",
-        "solved": false
+  threeHelpRequests: [
+    {
+      id: 1,
+      requesterEmail: "sburicha@ucsb.edu",
+      teamId: "s22-5pm-3\t",
+      tableOrBreakoutRoom: "5",
+      requestTime: "2022-04-20T17:35:00Z",
+      explanation: "DokkuIssue",
+      solved: false,
     },
-    
-    threeHelpRequests: [
-  {
-    "id": 1,
-    "requesterEmail": "sburicha@ucsb.edu",
-    "teamId": "s22-5pm-3\t",
-    "tableOrBreakoutRoom": "5",
-    "requestTime": "2022-04-20T17:35:00Z",
-    "explanation": "DokkuIssue",
-    "solved": false
-  },
-  {
-    "id": 2,
-    "requesterEmail": "samardh@ucsb.edu",
-    "teamId": "s22-6pm-4\t",
-    "tableOrBreakoutRoom": "8",
-    "requestTime": "2023-05-20T17:35:00Z",
-    "explanation": "GitHubActionsIssue",
-    "solved": true
-  },
-  {
-    "id": 3,
-    "requesterEmail": "achal@ucsb.edu",
-    "teamId": "s22-3pm-3",
-    "tableOrBreakoutRoom": "11",
-    "requestTime": "2025-05-21T17:35:00Z",
-    "explanation": "BackendTestProblem",
-    "solved": false
-  }
-],
-  };
-  
-  export { helpRequestFixtures };
-  
+    {
+      id: 2,
+      requesterEmail: "samardh@ucsb.edu",
+      teamId: "s22-6pm-4\t",
+      tableOrBreakoutRoom: "8",
+      requestTime: "2023-05-20T17:35:00Z",
+      explanation: "GitHubActionsIssue",
+      solved: true,
+    },
+    {
+      id: 3,
+      requesterEmail: "achal@ucsb.edu",
+      teamId: "s22-3pm-3",
+      tableOrBreakoutRoom: "11",
+      requestTime: "2025-05-21T17:35:00Z",
+      explanation: "BackendTestProblem",
+      solved: false,
+    },
+  ],
+};
+
+export { helpRequestFixtures };
