e0c5ae6a20ac0a0376f4abc91b1fd496053822d1
Josh1182021 <66659887+Josh1182021@users.noreply.github.com>
2025-05-02 17:28:02 -0700

    Fixed formatting

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,34 +1,33 @@
 const organizationFixtures = {
-    oneOrganization: [
-        {
-            "orgCode": "IEEE",
-            "orgTranslationShort": "Engineering",
-            "orgTranslation": "Institute of Electrical and Electronics Engineers",
-            "inactive": false
-        },
-    ],
-  
-    threeOrganizations: [
-        {
-            "orgCode": "DIVE",
-            "orgTranslationShort": "Diving",
-            "orgTranslation": "UCSB Diving Club",
-            "inactive": false
-            },
-            {
-            "orgCode": "GAME",
-            "orgTranslationShort": "Gaming",
-            "orgTranslation": "Gaucho Gaming",
-            "inactive": true
-            },
-            {
-            "orgCode": "NEXUS",
-            "orgTranslationShort": "Daily Nexus",
-            "orgTranslation": "Daily Nexus",
-            "inactive": false
-            },
-    ],
-  };
-  
-  export { organizationFixtures };
-  
+  oneOrganization: [
+    {
+      orgCode: "IEEE",
+      orgTranslationShort: "Engineering",
+      orgTranslation: "Institute of Electrical and Electronics Engineers",
+      inactive: false,
+    },
+  ],
+
+  threeOrganizations: [
+    {
+      orgCode: "DIVE",
+      orgTranslationShort: "Diving",
+      orgTranslation: "UCSB Diving Club",
+      inactive: false,
+    },
+    {
+      orgCode: "GAME",
+      orgTranslationShort: "Gaming",
+      orgTranslation: "Gaucho Gaming",
+      inactive: true,
+    },
+    {
+      orgCode: "NEXUS",
+      orgTranslationShort: "Daily Nexus",
+      orgTranslation: "Daily Nexus",
+      inactive: false,
+    },
+  ],
+};
+
+export { organizationFixtures };
