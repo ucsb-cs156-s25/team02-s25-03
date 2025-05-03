9095a9619d3a593fcf32dad3e973da93007834ff
Daisy <wanqian@ucsb.edu>
2025-05-03 00:59:28 -0700

    test

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -66,11 +66,11 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/recommendationrequests">
+                    Recommendation Request
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="/recommendationrequest">
-                    RecommendationRequest
                   </Nav.Link>
                 </>
               ) : (
