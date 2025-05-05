804671eec7e2cfb4c4d6e0625aec446ab4f27358
Karena Lai <65944717+klai890@users.noreply.github.com>
2025-05-05 18:04:50 -0400

    Fix createpage typo in params, fix broken link in navbar

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -58,7 +58,7 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/recommendationrequests">
+                  <Nav.Link as={Link} to="/recommendationrequest">
                     Recommendation Request
                   </Nav.Link>
                   <Nav.Link as={Link} to="/placeholder">
